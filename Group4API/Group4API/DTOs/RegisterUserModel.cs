7c066f9e8590990605569be88546927c5b13dd23
YerayRodriguez <yeray.rodriguez.7e5@itb.cat>
2025-05-26 12:29:38 +0200

    Register Fixed 2

--- Group4API/Group4API/DTOs/RegisterUserModel.cs
+++ Group4API/Group4API/DTOs/RegisterUserModel.cs
@@ -9,5 +9,7 @@
         [System.ComponentModel.DataAnnotations.Required]
         [System.ComponentModel.DataAnnotations.StringLength(100, MinimumLength = 6)]
         public string Password { get; set; }
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
